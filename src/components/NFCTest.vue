3133ffc521b4275ac574b189c96880bca1a696fe
Sananikone <lilian.sananikone@laposte.net>
2021-04-16 16:08:17 +0200

    updating my NFC tester

--- src/components/NFCTest.vue
+++ src/components/NFCTest.vue
@@ -1,28 +1,21 @@
 <template>
     <article>
-        <button onclick="readTag()">Test NFC Read</button>
-        <button onclick="writeTag()">Test NFC Write</button>
+        <button type="button" @click.prevent="writeTag">Test NFC Write</button>
+        <button type="button" @click.prevent="readTag">Test NFC Tag</button>
     </article>
 </template>
 
 <script>
     export default {
         methods: {
-            writeTag() {
-                if ("NDEFReader" in window) {
-                    const ndef = new NDEFReader();
-                    try {
-                        await ndef.write("What Web Can Do Today");
-                        consoleLog("NDEF message written!");
-                    }
-                    catch (error) {
-                        consoleLog(error);
-                    }
-                }
-                else {
-                    consoleLog("Web NFC is not supported.");
-                }
+            readTag() {
+                this.$nfc.runTagChecker()
             },
+            writeTag() {
+                // Format
+                // {userid};{points};{timestamp};{storeid}
+                this.$nfc.writeNdefTag('text', '')
+            }
         },
     };
 </script>
