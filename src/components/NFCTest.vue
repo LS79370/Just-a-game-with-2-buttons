8d9c0625b7b1a487b5dc4b93871b39df19262806
Sananikone <lilian.sananikone@laposte.net>
2021-04-16 14:24:00 +0200

    copying code from NFC doc

---
+++ src/components/NFCTest.vue
@@ -0,0 +1,28 @@
+<template>
+    <article>
+        <button onclick="readTag()">Test NFC Read</button>
+        <button onclick="writeTag()">Test NFC Write</button>
+    </article>
+</template>
+
+<script>
+    export default {
+        methods: {
+            writeTag() {
+                if ("NDEFReader" in window) {
+                    const ndef = new NDEFReader();
+                    try {
+                        await ndef.write("What Web Can Do Today");
+                        consoleLog("NDEF message written!");
+                    }
+                    catch (error) {
+                        consoleLog(error);
+                    }
+                }
+                else {
+                    consoleLog("Web NFC is not supported.");
+                }
+            },
+        },
+    };
+</script>
