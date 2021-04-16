c89559b0fad255cb755a6923090260226620edb0
Felix <banana.nordic@gmail.com>
2021-04-16 15:56:42 +0200

    Test de run de serv

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,4 @@
 <template>
-    <h1>Test de bouton</h1>
-    <p>I receive Money</p>
-    <p>
-        You <strong><em>receive</em></strong> Nothing <br>
-        Do you accept?
-    </p>
-    <div class="button">
-        <button id="button-yes" onclick="yes(); compte()">Accept</button>
-        <button id="button-no" onclick="no(); compte()">Refuse</button>
-        <p id="Resultat"> </p>
-        <div id="count"></div>
-    </div>
 </template>
 
 <script>
@@ -21,28 +9,7 @@ export default {
   components: {
     //HelloWorld,
   },
-    };
-
-    function yes() {
-        document.getElementById("Resultat").innerHTML = "Merci, nous nous reverrons jamais.";
-    }
-
-    function no() {
-        document.getElementById("Resultat").innerHTML = "Dommage, nous avions tellement à faire...";
-    }
-
-    function compte() {
-        if (typeof (Storage) !== "undefined") {
-            if (localStorage.clicked) {
-                localStorage.clicked = Number(localStorage.clicked) + 1;
-            }
-            else {
-                localStorage.clicked = 1;
-            }
-
-        }
-        document.getElementById("count").innerHTML = "Vous avez cliqué sur un bouton " + localStorage.clicked + " fois!";
-    }
+};
 </script>
 
 <style>
