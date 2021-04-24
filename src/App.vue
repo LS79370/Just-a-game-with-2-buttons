472936b2ef7944b827555997b69aa57ea434352e
Felix <banana.nordic@gmail.com>
2021-04-24 14:47:53 +0200

    Test de Bouton

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,16 @@
+<script>
+    new Vue({
+        el: '#test',
+        methods: {
+            action: function yes() {
+                
+            }
+        }
+    })
+
+</script>
+
+
 <template>
     <div class="hello">
         <h1>Test de bouton</h1>
@@ -7,33 +20,14 @@
             Do you accept?
         </p>
         <div class="button">
-            <button id="button-yes" onclick="yes(); compte()">Accept</button>
-            <button id="button-no" onclick="no(); compte()">Refuse</button>
-            <p id="Resultat"> </p>
-            <div id="count"></div>
+            <button id="button-yes" onclick="yes()">Accept</button>
+            <button id="button-no" onclick="no()">Refuse</button>
+            <p>{{R}} </p>
         </div>
     </div>
 </template>
 
-<script>
-    function yes() {
-        document.getElementById("Resultat").innerHTML = "Merci, nous nous reverrons jamais.";
-    }
-    function no() {
-        document.getElementById("Resultat").innerHTML = "Dommage, nous avions tellement à faire...";
-    }
-    function compte() {
-        if (typeof (Storage) !== "undefined") {
-            if (localStorage.clicked) {
-                localStorage.clicked = Number(localStorage.clicked) + 1;
-            }
-            else {
-                localStorage.clicked = 1;
-            }
-        }
-        document.getElementById("count").innerHTML = "Vous avez cliqué sur un bouton " + localStorage.clicked + " fois!";
-    }
-</script>
+
 
 <style>
     #app {
