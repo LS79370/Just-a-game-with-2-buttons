0c439649ca885d35cd1cbeffcd1883aae69afd1f
Sananikone <lilian.sananikone@laposte.net>
2021-04-24 15:21:01 +0200

    adding img folder

---
+++ src/components/imgDisplay.vue
@@ -0,0 +1,21 @@
+<template>
+    <article>
+        <img src="../assets/img/"
+             alt="Grapefruit slice atop a pile of other slices">
+    </article>
+</template>
+
+<script>
+    export default {
+        methods: {
+            readTag() {
+                this.$nfc.runTagChecker()
+            },
+            writeTag() {
+                // Format
+                // {userid};{points};{timestamp};{storeid}
+                this.$nfc.writeNdefTag('text', '')
+            }
+        },
+    };
+</script>
