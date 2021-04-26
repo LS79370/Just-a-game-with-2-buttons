b9b9b06699cdf5adc5464587ab4f2ed696517259
Felix <banana.nordic@gmail.com>
2021-04-26 19:28:49 +0200

    Modif de la page

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@
                     require(".\\assets\\img\\sweat.png"),
                     require(".\\assets\\img\\thumbup.png")
                 ],
-                pilote : [],
+                pilote: [],
             }
         },
         mounted() {
@@ -35,15 +35,50 @@
 </script>
 
 <template>
-    <div class="hello">
-        <h1>Test de bouton</h1>
-        <img v-bind:src="test" alt="image" /> <br />
-        <button class="test" v-on:click="boup">Boutons</button>
-        <button class="test" v-on:click="face">Pilote</button>
+    <div class="body">
+        <head>
+            <meta charset="utf-8" />
+            <link rel="preconnect" href="https://fonts.gstatic.com">
+            <link href="https://fonts.googleapis.com/css2?family=Space+Mono&display=swap" rel="stylesheet">
+        </head>
+        <header>
+            <img class="icon" src=".\\assets\\logo.png" alt="logo" />
+            <h1>Juste un jeu avec 2 boutons...</h1>
+        </header>
+        <div class="game-scene">
+            <pre><h2>Atteindrez-vous la planète Mars ?</h2></pre>
+            <pre>Finalement, vous n'avez pas assez de vivres pour tout l'équipage... il vous faut éjecter un membre (-1) ou consommer plus de carburant pour aller faire les courses(-2).</pre>
+            <div class="game-ui">
+                <img v-bind:src="test" alt="capitaine" />
+                <div class="controller">
+                    <ul>
+                        <li>
+                            <img class="icon" src=".\\assets\\img\\crewmate.png" alt="crew" />
+                            <h2>
+                                15
+                            </h2>
+                        </li>
+                        <li>
+                            <img class="icon" src=".\\assets\\img\\lightning.png" alt="spaceship" />
+                            <h2>
+                                20
+                            </h2>
+                        </li>
+                    </ul>
+                    <button v-on:click="pouf">Ejecter un membre</button>
+                    <button v-on:click="face">Faire les courses</button>
+                </div>
+            </div>
+        </div>
+        <footer>
+            <pre>
+                Jeu développé par Félix Delesalle et Lilian Sananikone, étudiants au CNAM-ENJMIN.
+            </pre>
+        </footer>
     </div>
 </template>
 
 <style>
-    app{
-    }
+    @import url('https://fonts.googleapis.com/css2?family=Space+Mono&display=swap');
+    @import '.\\stylesheets\\style.css';
 </style>
