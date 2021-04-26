35a9b1dacd81b3b345e97f88e3b1340810cc6db6
Felix <banana.nordic@gmail.com>
2021-04-26 18:08:16 +0200

    Ajouts de swap d'images

--- src/App.vue
+++ src/App.vue
@@ -3,31 +3,33 @@
         el: "#hello",
         data() {
             return {
-                result: "",
-                test: require(".\\assets\\img\\sweat.png"),
+                test: "", //variable qui affiche les images. elle prend une valeur dans boup et face
                 index: 0,
-                image: null,
-                items: [
-                    { test: require(".\\assets\\img\\choice.png") },
-                    { test: require(".\\assets\\img\\sweat.png") },
-                    { test: require(".\\assets\\img\\thumbup.png") },
-                    { test: require(".\\assets\\img\\twice.png") }
-                ]
+                items: [ //liste des images
+                    require(".\\assets\\img\\choice.png"),
+                    require(".\\assets\\img\\twice.png"),
+                    require(".\\assets\\img\\sweat.png"),
+                    require(".\\assets\\img\\thumbup.png")
+                ],
+                pilote : [],
             }
         },
+        mounted() {
+            this.cycle();
+        },
         methods: {
-            yes: function () {
-                this.result = "Merci de ton argent";
+            cycle: function () { // sert à initialiser l'image, que y'ai pas un trou
+                this.test = this.items[this.index];
             },
-            no: function () {
-                this.result = "Haha, perdu";
+            boup: function () { // affiche que les images des bouton : choice et twice
+                this.index = (this.index + 1) % 2;
+                this.test = this.items[this.index];
             },
-
-            pouf: function () {
-                this.image = this.items[this.index];
-                this.result = this.index;
-                this.index = (this.index + 1) % this.items.length;
-            },
+            face: function () { //affiche que le pilote sweat et thumbup
+                this.pilote = this.items.slice(2);
+                this.index = (this.index + 1) % 2;
+                this.test = this.pilote[this.index];
+            }
         }
     };
 </script>
@@ -35,18 +37,9 @@
 <template>
     <div class="hello">
         <h1>Test de bouton</h1>
-        <p>I receive Money</p>
-        <p>
-            You <strong><em>receive</em></strong> Nothing <br>
-            Do you accept?
-        </p>
-        <div class="button">
-            <button v-on:click="yes">Accept</button>
-            <button v-on:click="no">Refuse</button>
-            <p>{{ result }}</p>
-        </div>
-        <img src=".\assets\img\choice.png" alt="Choix" /> <br />
-        <button class="test" v-on:click="pouf">Bouton</button>
+        <img v-bind:src="test" alt="image" /> <br />
+        <button class="test" v-on:click="boup">Boutons</button>
+        <button class="test" v-on:click="face">Pilote</button>
     </div>
 </template>
 
