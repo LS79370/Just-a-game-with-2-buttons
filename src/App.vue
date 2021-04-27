ddcb0a99fece4c5792f831cad2d6a4ec77db8851
Felix <banana.nordic@gmail.com>
2021-04-27 15:02:18 +0200

    Conteurs
    Boutons font baisser valeurs, boutons ne restent pas cliqués

--- src/App.vue
+++ src/App.vue
@@ -3,15 +3,16 @@
         el: "#hello",
         data() {
             return {
-                test: "", //variable qui affiche les images. elle prend une valeur dans boup et face
+                resultat: "", //variable qui affiche les images. elle prend une valeur dans boup et face
                 index: 0,
+                membres: 15,
+                carburant : 20,
                 items: [ //liste des images
                     require(".\\assets\\img\\choice.png"),
                     require(".\\assets\\img\\twice.png"),
                     require(".\\assets\\img\\sweat.png"),
                     require(".\\assets\\img\\thumbup.png")
                 ],
-                pilote: [],
             }
         },
         mounted() {
@@ -19,16 +20,25 @@
         },
         methods: {
             cycle: function () { // sert � initialiser l'image, que y'ai pas un trou
-                this.test = this.items[this.index];
+                this.resultat = this.items[this.index];
             },
-            boup: function () { // affiche que les images des bouton : choice et twice
-                this.index = (this.index + 1) % 2;
-                this.test = this.items[this.index];
+            bouton1: function () { // affiche que les images des bouton : choice et twice
+                this.resultat = this.items[2];
+                if (this.membres > 0) {
+                    this.membres = this.membres - 1;
+                }
+                else {
+                    this.membres = 0;
+                }
             },
-            face: function () { //affiche que le pilote sweat et thumbup
-                this.pilote = this.items.slice(2);
-                this.index = (this.index + 1) % 2;
-                this.test = this.pilote[this.index];
+            bouton2: function () { //affiche que le pilote sweat et thumbup
+                this.resultat = this.items[3];
+                if (this.carburant > 0) {
+                    this.carburant = this.carburant - 2;
+                }
+                else {
+                    this.carburant = 0;
+                }
             }
         }
     };
@@ -46,27 +56,27 @@
             <h1>Juste un jeu avec 2 boutons...</h1>
         </header>
         <div class="game-scene">
-            <pre><h2>Atteindrez-vous la plan�te Mars ?</h2></pre>
-            <pre>Finalement, vous n'avez pas assez de vivres pour tout l'�quipage... il vous faut �jecter un membre (-1) ou consommer plus de carburant pour aller faire les courses(-2).</pre>
+            <pre><h2>Atteindrez-vous la planète Mars ?</h2></pre>
+            <pre>Finalement, vous n'avez pas assez de vivres pour tout l'équipage... il vous faut éjecter un membre (-1) ou consommer plus de carburant pour aller faire les courses(-2).</pre>
             <div class="game-ui">
-                <img v-bind:src="test" alt="capitaine" />
+                <img v-bind:src="resultat" alt="capitaine" />
                 <div class="controller">
                     <ul>
                         <li>
                             <img class="icon" src=".\\assets\\img\\crewmate.png" alt="crew" />
                             <h2>
-                                15
+                                {{membres}}
                             </h2>
                         </li>
                         <li>
                             <img class="icon" src=".\\assets\\img\\lightning.png" alt="spaceship" />
                             <h2>
-                                20
+                                {{carburant}}
                             </h2>
                         </li>
                     </ul>
-                    <button v-on:click="pouf">Ejecter un membre</button>
-                    <button v-on:click="face">Faire les courses</button>
+                    <button v-on:click="bouton1">Ejecter un membre</button>
+                    <button v-on:click="bouton2">Faire les courses</button>
                 </div>
             </div>
         </div>
