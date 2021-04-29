c7e757fa5c26275861115f9a51f1e0b50e72c6df
Felix <banana.nordic@gmail.com>
2021-04-29 19:06:30 +0200

    Non-repeat d'events

--- src/App.vue
+++ src/App.vue
@@ -12,26 +12,25 @@
                     require(".\\assets\\img\\thumbup.png")
                 ],
                 missnbr: 0, //indice de la mission, qui syncronise les coûts et les boutons
+                previous: 0,
                 mission: "",
                 missionlist: [ //liste des missions
-                    "Votre but est d'atteindre la planète Mars sans perdre tout votre équipage ou tous les éléments de votre vaisseau. Pour commencer, faites décoler le vaiseau (-1)!",
+                    "Votre but est d'atteindre la planète Mars sans perdre la totalité des memebres de votre équipage ou celle des éléments de votre vaisseau. Pour commencer, faites décoller le vaiseau (-1)!",
                     "Ah mince ! Vous n'avez pas attendu Patrick, il est resté sur Terre. Faites demi-tour (-2) ou ignorez-le (-1)...",
                     "Une brèche dans le compartiment P6 du vaisseau ! Emma s'y trouve, elle va être aspirée dans l'espace... vous pouvez la sauver (-3) ou fermer le sas (-1) et présuriser l'engin.",
                     "Un alien ravageur se trouve dans la chambre de Thibal ! Il vous faut condamner Thibal et sa chambre (-1 -1) ou permettre à Thibal de s'échapper à vos risques et périls. (-3)",
-                    "Un virus informique brouille l'image des caméras de surveillance, le Novid. Envoyer Florian, l'expert informatique, en salle de contrôle (-1 -1) ou laisser l'anti-virus Javast agir (-2).",
-                    "4 cabines d'hybernation sont en panne, des membres de l'équipage propose de laisser leur places mais il peuvent mourir d'insomnie spatiale. Acceptez leur proposition (-4) ou faites réparer les cabines (-3).",
-                    /* Au lieu de tout le temps retirer des ressources, en ajouter? le vendeur redonne du carburant non?*/
-                    "Vous n'avez plus assez de carburant mais à ce stade, vous ne pouvez que vous en procurer au marché rouge ! Un vendeur exige 3 membres de votre équipage contre du carburant (-3) ... si vous n'acceptez pas l'offre votre vaisseau sera vite en panne (-3).",
-
-                    "Vous n’avez pas assez de vivres pour tout l’équipage… il vous faut éjecter un membre(-1) ou consommer plus de carburant pour aller faire les courses(-2).",
-                    "2 membres de votre équipe nuisent à l'ambiance du groupe, mais ils s'agissent des ouvriers en chef de l'aile est du vaisseau. A vous de choisir quoi sacrifier entre la cohésion du groupe (-2) et l'entretien du vaisseau(-3)...",
-                    "Votre supérieur en chef, Tifanie, vous demande un rapport intermédiaire de mission mais votre Holophone est cassé... Vous pouvez envoyer 2 subordonnés au spatioport pour un compte-rendu (-2) ou faire réparer (-1) votre appareil de communication ! ",
+                    "Un virus informique brouille l'image des caméras de surveillance, le Novid. Envoyer Florian, l'expert informatique, en salle de contrôle (-1 -1) ou laisser l'anti-virus Javast agir (-2)?",
+                    "4 cabines d'hybernation sont en panne, des membres de l'équipage proposent de laisser leur places mais ils peuvent mourir d'insomnie spatiale. Acceptez leur proposition (-4) ou faites réparer les cabines (-3).",
+                    "Vous n'avez plus assez de carburant mais à ce stade, vous ne pouvez que vous en procurer au marché rouge ! Un vendeur exige 3 membres de votre équipage contre du carburant (-3) ... Si vous n'acceptez pas l'offre votre vaisseau sera vite en panne (-3).",
+                    "Vous n’avez pas assez de vivres pour tout l’équipage… Il vous faut éjecter un membre(-1) ou consommer plus de carburant pour aller faire les courses(-2).",
+                    "2 membres de votre équipe nuisent à l'ambiance du groupe, mais ils s'agit des ouvriers en chef de l'aile est du vaisseau. A vous de choisir quoi sacrifier entre la cohésion du groupe (-2) et l'entretien du vaisseau(-3)...",
+                    "Votre supérieure en chef, Tifanie, vous demande un rapport intermédiaire de mission mais votre Holophone est cassé... Vous pouvez envoyer 2 subordonnés au spatioport pour un compte-rendu (-2) ou faire réparer (-1) votre appareil de communication ! ",
                     "Toutes les innovations présentes sur la plateau Projet du vaisseau ont pris feu ! 4 membres de votre équipage sont au coeur de l'incendie, sauvez-les (-1) ! Ou profitez-en pour faire des licenciements (-4) !",
-                    "Vous êtes attaqués par un raton-laveur et un tréant de l'espace. Envoyez votre jardinier zoologiste (-1) ou utilisez vos canons (-2).",
+                    "Vous êtes attaqués par un raton-laveur et un tréant de l'espace. Envoyez votre jardinier zoologiste (-1) ou utilisez vos canons (-2)!",
                     "Ulysse a perdu contact avec l'un de ses 31 drones caméra. Il vous propose d'aller le retrouver personnellement (-1) à ses dernières coordonnées GPS au lieu d'utiliser un autre drone et risquer de le perdre (-1) Que choisissez-vous ?",
                     "Le compartiment P17 de votre vaisseau est fortement endommagé, certains membre de l'équipage y sont bloqués et risque de mourir d'asphyxie. Heureusement, ils sont prêts des cabines de sauvetages. Autorisez-les à embarquer dans les cabines (-3) ou attendez que l'accès au compartement soit réparé (-5).",
-                    "Vous arrivez presque à destination : un martien s'échappe du vaisseau avec les chips préférées de 3 vos subordonnés! Mais il va dans la direction opposée du vaisseau... faites demi-tour (-3) pour faire plaisir à votre équipage ou laissez-les y aller seuls (-3).",
-                    "Il y a une mutinerie dans votre vaisseau, protégez-vous en condamnant l'accès au pont (-2) ou laissez-les vous approcher et tuez les mutins (-4).",
+                    "Vous arrivez presque à destination : un martien s'échappe du vaisseau avec les chips préférées de 3 de vos subordonnés! Mais il va dans la direction opposée du vaisseau... faites demi-tour (-3) pour faire plaisir à votre équipage ou laissez-les y aller seuls (-3).",
+                    "Il y a une mutinerie dans votre vaisseau, protégez-vous en condamnant l'accès au pont (-2) ou laissez-les approcher et tuez les mutins (-4)!",
                 ],
                 CB1: [0, -1, -1, -1, -1, -4, -3, -1, -2, -2, -4, -1, -1, -3, -3, -4], //listes des prix
                 CB2: [-1, -2, -3, -3, -2, -3, -3, -2, -3, -1, -1, -2, -1, -5, -3, -2],
@@ -49,7 +48,7 @@
                     "Envoyer les subordonnés",
                     "Licencier",
                     "Jardinier Zoologiste",
-                    "Chercher le drone",
+                    "Envoyer Ulysse",
                     "Les capsules!",
                     "Qu'ils y aillent seuls",
                     "AU COMBAT!",
@@ -88,7 +87,6 @@
                 this.mission = this.missionlist[this.missnbr];
                 this.TXTB1 = this.TXT1[this.missnbr];
                 this.TXTB2 = this.TXT2[this.missnbr];
-
             },
             Button1: function () { //paye le coût en members, vérifie si les members est supp à 0, si oui, continue, sinon echec. Si oui, décrémente sauts, jusqu'à victoire
                 if (this.missnbr == 3 || this.missnbr == 4 || this.missnbr == 0) {
@@ -119,8 +117,14 @@
                     }
                     else {
                         this.sauts = this.sauts - 1;
+                        this.previous = this.missnbr;
                         this.missnbr = Math.floor(Math.random() * (15 - 1 + 1)) + 1;
-                        this.cycle();
+                        if (this.previous == this.missnbr || this.missnbr == 0) {
+                            this.missnbr = Math.floor(Math.random() * (15 - 1 + 1)) + 1;
+                        }
+                        else {
+                            this.cycle();
+                        }
                     }
                 }
             },
@@ -138,15 +142,21 @@
                     }
                     else {
                         this.sauts = this.sauts - 1;
+                        this.previous = this.missnbr;
                         this.missnbr = Math.floor(Math.random() * (15 - 1 + 1)) + 1;
-                        this.cycle();
+                        if (this.previous == this.missnbr || this.missnbr == 0) {
+                            this.missnbr = Math.floor(Math.random() * (15 - 1 + 1)) + 1;
+                        }
+                        else {
+                            this.cycle();
+                        }
                     }
                 }
             },
             Victoire: function () { //Vous avez gagné
                 this.resultat = this.items[3];
                 this.Titre = "VICTOIRE!";
-                this.mission = "Vous avez réussi! Vous êtes arrivés à atteidre la planète Mars! Certes, vous avez perdu des compagnons, et votre vaisseau ressemble à une épave. Mais, vous avez réussi!";
+                this.mission = "Vous avez réussi! Vous êtes arrivés à atteidre la planète Mars! La planète rouge est à vous! Vous serez inscrit dans l'histoire!";
                 this.TXTB1 = this.TXT2[16];
                 this.TXTB2 = this.TXT2[16];
             },
@@ -167,8 +177,12 @@
                 if (this.sauts == 0) {
                     this.resultat = this.items[3];
                 }
-            }
-        }
+            },
+            Twice: function () {
+                this.resultat = this.items[1];
+        },
+        
+        },
     };
 </script>
 
@@ -207,7 +221,9 @@
                     <div class="buttons">
                         <button v-on:click="Button1" v-on:mouseover="Tension" v-on:mouseleave="Part">{{TXTB1}}</button>
                         <button v-on:click="Button2" v-on:mouseover="Tension" v-on:mouseleave="Part">{{TXTB2}}</button>
+                        <button v-on:click="Button2" v-on:mouseover="Twice" v-on:mouseleave="Part">Une autre option?</button>
                     </div>
+                    
                 </div>
             </div>
         </div>
