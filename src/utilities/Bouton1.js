7babef4560c3ca407d431e06bc614dcd1dfbfcfa
Felix <banana.nordic@gmail.com>
2021-04-29 20:27:52 +0200

    Ajout de fichier des textes boutons, appel dans app.vue

---
+++ src/utilities/Bouton1.js
@@ -0,0 +1,37 @@
+function initiateButton1() {
+
+    var txt0 = "En route vers l'aventure!";
+
+    var txt1 = "Ignorer Patrick";
+
+    var txt2 = "Présurisez l'engin";
+
+    var txt3 = "Condamner Thibal";
+
+    var txt4 = "Envoyer Florian";
+
+    var txt5 = "Accepter leur proposition";
+
+    var txt6 = "Acheter le carburant";
+
+    var txt7 = "Ejecter un membre";
+
+    var txt8 = "La cohésion";
+
+    var txt9 = "Envoyer les subordonnés";
+
+    var txt10 = "Licencier";
+
+    var txt11 = "Jardinier Zoologiste";
+
+    var txt12 = "Envoyer Ulysse";
+
+    var txt13 = "Les capsules!";
+
+    var txt14 = "Qu'ils y aillent seuls";
+
+    var txt15 = "AU COMBAT!";
+
+    return [txt0, txt1, txt2, txt3, txt4, txt5, txt6, txt7, txt8, txt9, txt10, txt11, txt12, txt13, txt14, txt15]
+}
+export default initiateButton1
