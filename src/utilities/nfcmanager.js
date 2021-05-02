735813c0185eb9d8414169fd5dae7898db24ba41
Mogador <jcaillaud17170@gmail.com>
2021-05-02 14:10:26 +0200

    Change to Promise

--- src/utilities/nfcmanager.js
+++ src/utilities/nfcmanager.js
@@ -1,25 +1,26 @@
 async function readNFC()
 {
-    if ("NDEFReader" in window) {
-        //si l'appareil est compatible NFC
+    return new Promise((resolve, reject) => {
+        if ("NDEFReader" in window) {
+            //si l'appareil est compatible NFC
 
-        
-        const ndef = new NDEFReader();
-        /*global NDEFReader*/
-        try {
-            await ndef.scan();//attend que l'appareil scanne une puce NFC
+            /*global NDEFReader*/
+            const ndef = new NDEFReader();
             ndef.onreading = (event) => {
                 const decoder = new TextDecoder();
                 let record = event.message.records[0];//on r�cup�re le message enregistr� dans la puce
                 console.log(record.data);
                 var textTag= decoder.decode(record.data);
-                return textTag === "Passe"                
+                resolve(textTag === "Passe");               
             }
+            ndef.addEventListener("readingerror", () => {
+                reject(" Impossible de lire le Tag NFC");
+            });
+            ndef.scan();//attend que l'appareil scanne une puce NFC
+        } else {
+            reject(" Utilisation du joker : Appareil incompatible ou permission refusée !");
         }
-        catch (error) {
-            console.log(error);
-        }
-    }
+    });
 }
 
 export default readNFC;
